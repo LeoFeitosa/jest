a72b04ceee98e67257c8141a20f0df72c1d523c8
LeoFeitosa <>
2021-10-29 18:02:27 -0300

    add prettier

--- PROJETO1/src/lib/queryString.spec.js
+++ PROJETO1/src/lib/queryString.spec.js
@@ -1,14 +1,12 @@
 const { queryString } = require('./queryString');
 
 describe('Objeto em string', () => {
-    it('should criar uma query string valida com um objeto', () => {
-        const obj = {
-            name: 'Leonardo',
-            profession: 'Developer'
-        }
+  it('should criar uma query string valida com um objeto', () => {
+    const obj = {
+      name: 'Leonardo',
+      profession: 'Developer',
+    };
 
-        expect(queryString(obj)).toBe(
-            'name=Leonardo&profession=Developer'
-        );
-    });
+    expect(queryString(obj)).toBe('name=Leonardo&profession=Developer');
+  });
 });
